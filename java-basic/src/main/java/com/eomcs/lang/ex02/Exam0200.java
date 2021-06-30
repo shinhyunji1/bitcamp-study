72c136946a76a58f418141e26563705ad397bf26
shinhyunji1 <shji0814@gmail.com>
2021-06-30 20:53:41 +0900

    .

---
+++ java-basic/src/main/java/com/eomcs/lang/ex02/Exam0200.java
@@ -0,0 +1,8 @@
+package com.eomcs.lang.ex02;
+
+public class Exam0200{
+    public static String message = "Hello, World!";
+    public static void main(String[] args){
+        System.out.println(message);
+    }
+}
