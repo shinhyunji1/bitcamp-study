cce10be19715bd07a98ad68976930b7c13ac60ee
shji0814@gmail.com <shinhyunji1>
2021-07-02 18:39:32 +0900

    .

--- java-basic/src/main/java/Hello.java
+++ java-basic/src/main/java/Hello.java
@@ -1,15 +1,12 @@
 class Hello {
     //---------------------------
-    // | main()                  |
-    // | - 프로그램 실행 시작점  |
-    // | - 'entry point' 라 부름 |
+    // | main()                   |
+    // | - 프로그램 실행 시작점   |
+    // | - 'entry point' 라 부름  |
     //---------------------------
     public static void main(String [] args){
         System.out.println("Hello, World!");
-<<<<<<< HEAD
         System.out.println("안녕하세요!");
-=======
-         System.out.println("안녕하세요!");
->>>>>>> e0b7f033c5b91a75f883e8d2e46c290886ab5910
+        System.out.println("안녕하세요!");
     }
 }
