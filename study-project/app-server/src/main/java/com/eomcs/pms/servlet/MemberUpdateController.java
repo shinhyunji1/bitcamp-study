2f5875877428b8f3d8c59645a1caff988b76b630
shinhyunji1 <shji0814@gmail.com>
2021-10-30 20:45:49 +0900

    jsp

--- study-project/app-server/src/main/java/com/eomcs/pms/servlet/MemberUpdateController.java
+++ study-project/app-server/src/main/java/com/eomcs/pms/servlet/MemberUpdateController.java
@@ -1,7 +1,6 @@
 package com.eomcs.pms.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -13,9 +12,8 @@ import org.apache.ibatis.session.SqlSession;
 import com.eomcs.pms.dao.MemberDao;
 import com.eomcs.pms.domain.Member;
 
-
-@WebServlet("/member/add")
-public class MemberAddHandler extends HttpServlet {
+@WebServlet("/member/update")
+public class MemberUpdateController extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
   SqlSession sqlSession;
@@ -32,43 +30,30 @@ public class MemberAddHandler extends HttpServlet {
   protected void service(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 
-    response.setContentType("text/html;charset=UTF-8");
-    PrintWriter out = response.getWriter();
-
-    out.println("<html>");
-    out.println("<head>");
-    out.println("  <title>회원등록</title>");
-    out.println("</head>");
-    out.println("<body>");
-    out.println("<h1>회원등록결과</h1>");
-
-    Member member = new Member();
-
-    member.setName(request.getParameter("name"));
-    member.setEmail(request.getParameter("email"));
-    member.setPassword(request.getParameter("password"));
-    member.setPhoto(request.getParameter("photo"));
-    member.setTel(request.getParameter("tel"));
-
     try {
-      memberDao.insert(member);
+      int no = Integer.parseInt(request.getParameter("no"));
+
+      Member member = memberDao.findByNo(no);
+
+      if (member == null) {
+        throw new Exception("해당 번호의 회원이 없습니다.");
+      } 
+
+      member.setName(request.getParameter("name"));
+      member.setEmail(request.getParameter("email"));
+      member.setPassword(request.getParameter("password"));
+      member.setPhoto(request.getParameter("photo"));
+      member.setTel(request.getParameter("tel"));
+
+      memberDao.update(member);
       sqlSession.commit();
 
-      out.println("회원을 등록했습니다.<br>");
-
-      out.println("<a href='list'>[목록]</a><br>");
+      response.sendRedirect("list");
 
     } catch (Exception e) {
-      out.println("목록 조회 오류!");
-      throw new ServletException(e);
+      request.setAttribute("error", e);
+      request.getRequestDispatcher("/Error.jsp").forward(request, response);
     }
-
-    out.println("</body>");
-    out.println("</html>");
-
-    // 웹
-    // 1초가
-    response.setHeader("Refresh", "1;url=list");
   }
 }
 
--- study-project/app-server/src/main/webapp/member/MemberList.jsp
+++ study-project/app-server/src/main/webapp/member/MemberList.jsp
@@ -1,6 +1,5 @@
 <%@page import="com.eomcs.pms.domain.Member"%>
 <%@page import="java.util.Collection"%>
-<%@page import="com.eomcs.pms.dao.MemberDao"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"
     trimDirectiveWhitespaces="true" %>
@@ -10,8 +9,8 @@
   <title>회원목록</title>
 </head>
 <body>
-<h1>회원 목록</h1>
-<a href='MemberForm.jsp'>새회원</a><br>
+<h1>회원 목록(MVC)</h1>
+<a href='form'>새회원</a><br>
 <table border='1'>
 <thead>
   <tr>
@@ -23,14 +22,14 @@
   </tr>
 </thead>
 <tbody>
-<% // <== scriptlet (자바 코드 조각을 두는 태그)
-Collection<Member> memberList = memberDao.findAll();
+<% 
+Collection<Member> memberList = (Collection<Member>) request.getAttribute("memberList");
 
 for (Member member : memberList) {
 %>
 <tr>
     <td><%=member.getNo()%></td>
-    <td><a href='MemberDetail.jsp?no=<%=member.getNo()%>'><%=member.getName()%></a></td> 
+    <td><a href='detail?no=<%=member.getNo()%>'><%=member.getName()%></a></td> 
     <td><%=member.getEmail()%></td> 
     <td><%=member.getTel()%></td> 
     <td><%=member.getRegisteredDate()%></td>
@@ -40,17 +39,6 @@ for (Member member : memberList) {
 </table>
 </body>
 </html>
-<%! // <== declaration element(tag)
-// 자바 서블릿 클래스를 만들 때 그 클래스에 들어갈 변수와 메서드를 이 태그 안에 작성한다.
-    MemberDao memberDao;
-
-    public void jspInit() {
-      ServletConfig config = getServletConfig();
-      ServletContext 웹애플리케이션공용저장소 = config.getServletContext();
-      memberDao = (MemberDao) 웹애플리케이션공용저장소.getAttribute("memberDao");
-    }
-%>
-
 
 
 
